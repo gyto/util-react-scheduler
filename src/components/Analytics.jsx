396810fc377c248188cea29eb2ab32f3075fad07
Roman Kovtun <roman.kovtun@webtomed.com>
2019-03-04 16:08:51 -0600

    Improve chart

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -11,7 +11,7 @@ class Analytics extends React.Component<{}> {
                 {
                     label: 'My First dataset',
                     fill: false,
-                    lineTension: 0.1,
+                    lineTension: 0,
                     backgroundColor: 'rgba(75,192,192,0.4)',
                     borderColor: 'rgba(75,192,192,1)',
                     borderCapStyle: 'butt',
@@ -27,9 +27,21 @@ class Analytics extends React.Component<{}> {
                     pointHoverBorderWidth: 2,
                     pointRadius: 1,
                     pointHitRadius: 10,
-                    data: [65, 59, 80, 81, 56, 55, 40],
+                    data: [10, 59, 80, 81, 56, 55, 40],
                 },
-            ],
+                {
+                    label: 'My second dataset',
+                    fill: false,
+                    data: [40, 55, 56, 81, 80, 59, 10],
+                    // lineTension: 0,
+                },
+                {
+                    label: 'My third dataset',
+                    fill: false,
+                    data: [69, 49, 85, 93, 15, 12, 27],
+                    // lineTension: 0,
+                },
+            ]
         };
 
         return (
