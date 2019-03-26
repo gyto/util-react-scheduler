0a57dee1958286a74a698ba3114210a264f62916
Roman Kovtun <roman.kovtun@webtomed.com>
2019-03-26 15:03:44 -0500

    Cleanup

--- src/reducers/Cart.js
+++ src/reducers/Cart.js
@@ -1,31 +1,19 @@
 // @flow
 
-import { UPDATE_CART } from '../actions/UpdateCart';
-import { ADD_TO_CART, SET_CART } from '../actions/SetCart';
 
-function getCart (newCriteria, state = {}) {
-    const productId = { ...state, ...newCriteria };
-    for (let key in productId) {
-        if (!productId.hasOwnProperty(key)) {
-            continue;
-        }
-        if (!productId[key]) delete productId[key];
-    }
-    return productId;
-}
+import { ADD_TO_CART } from '../actions/SetCart';
 
-const initialState = {
+type initialState = {}
+
+const State: initialState = {
     productId: [],
 };
 
 export default (
-    state = initialState,
+    state: initialState = State,
     action
 ) => {
     switch (action.type) {
-        case UPDATE_CART:
-            return getCart(action.productId, state);
-
         case ADD_TO_CART:
             if (state.productId.indexOf(action.productId) !== -1) {
                 return state;
