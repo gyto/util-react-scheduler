5d14ec58f5260aaed59b5163c9d4c01139539a61
Roman Kovtun <gyto23@gmail.com>
2019-04-02 00:08:20 -0500

    Update events

--- src/components/Scheduler/Events.jsx
+++ src/components/Scheduler/Events.jsx
@@ -9,6 +9,11 @@ type Props = {}
 
 type State = {}
 
+/**
+ * TODO: create event cards
+ * TODO: should accept {name, startIn(date, time), endsIn(date, time), serviceId, status, appxTotal }
+ */
+
 class Events extends React.Component<Props, State> {
     render() {
         return (
